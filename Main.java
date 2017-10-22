bce3d89047f1d9379178a1319d41e68f45242990
Jose Ortiz <joseortizlack@outlook.com>
2017-10-21 21:53:50 -0500

    Limpieza de codigo y funcion para crear matriz identidad agregada

--- Main.java
+++ Main.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author Heloel Hernandez Santos A07007415
@@ -12,9 +6,6 @@
 import java.util.Scanner;
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static Fraccion[][] A;
     public static Fraccion[][] B;
     public static void main(String[] args) {
@@ -24,8 +15,10 @@ public class Main {
         A = new Fraccion[n][n];
         B = new Fraccion[n][n];
         for (int i = 0; i < n; i++) {
+            
             for (int j = 0; j < n; j++) {
-              A[i][j] = new Fraccion(Integer.parseInt(input.nextLine()),Integer.parseInt(input.nextLine()));
+                
+                A[i][j] = new Fraccion(Integer.parseInt(input.nextLine()),Integer.parseInt(input.nextLine()));
             }
         }
         
@@ -35,6 +28,7 @@ public class Main {
         imprimeMatriz(A);
         B=A;
         for (int i = 2; i <= k; i++) {
+            
             System.out.println("P"+i);
             B = multiplicaMatriz(A,B);
             imprimeMatriz(simplificaMatriz(B));
@@ -46,8 +40,10 @@ public class Main {
     }
     
     public static Fraccion multiplicaFraccion(Fraccion uno, Fraccion dos){
+        
         return new Fraccion(uno.getNumerador()*dos.getNumerador(), uno.getDenominador()*dos.getDenominador());
     }
+
     public static void imprimeMatriz(Fraccion[][] A){
         for (int i = 0; i < A.length; i++) {
             for (int j = 0; j < A.length; j++) {
@@ -56,25 +52,34 @@ public class Main {
             System.out.println("");
         }
     }
+
     public static Fraccion[][] simplificaMatriz(Fraccion[][] A){
+        
         Fraccion[][] temporal = matrizDeCeros(A.length);
         for (int i = 0; i < A.length; i++) {
+            
             for (int j = 0; j < A.length; j++) {
-               temporal[i][j]=simplifica(A[i][j]);
+               
+                temporal[i][j]=simplifica(A[i][j]);
             }
         }
         return temporal;
     }
+
     public static void imprimeFraccion(Fraccion uno){
+        
         System.out.print(" "+uno.getNumerador()+"/"+uno.getDenominador()+" ");
     }
     
     public static Fraccion[][] multiplicaMatriz(Fraccion[][] A, Fraccion[][] B){
+        
         Fraccion[][] temporal = matrizDeCeros(A.length);
         for (int i = 0; i < A.length; i++)
         {
+            
             for (int j = 0; j < A.length; j++)
             {
+                
                 for (int k = 0; k < A.length; k++)
                 {
                     temporal[i][j] = sumaFraccion(temporal[i][j], multiplicaFraccion(A[i][k], B[k][j]));
@@ -83,25 +88,50 @@ public class Main {
         }
         return temporal;
     }
-    public static Fraccion[][] matrizDeCeros(int longitud){
+
+    public static Fraccion[][] matrizIdentidad(int longitud){
+        
         Fraccion[][] temporal = new Fraccion[longitud][longitud];
         for (int i = 0; i < longitud; i++) {
+            
             for (int j = 0; j < longitud; j++) {
+                if(i == j) {
+                    temporal[i][j] = new Fraccion(1,1);
+                }
+                else {
+                    temporal[i][j] = new Fraccion(0,1);
+                }
+            }
+        }
+        return temporal;
+    }
+
+    public static Fraccion[][] matrizDeCeros(int longitud){
+        
+        Fraccion[][] temporal = new Fraccion[longitud][longitud];
+        for (int i = 0; i < longitud; i++) {
+            
+            for (int j = 0; j < longitud; j++) {
+                
                 temporal[i][j] = new Fraccion(0,1);
             }
         }
         return temporal;
     }
+
     public static Fraccion sumaFraccion(Fraccion uno, Fraccion dos){
-            return new Fraccion((uno.getNumerador()*dos.getDenominador())+(dos.getNumerador()*uno.getDenominador()), uno.getDenominador()*dos.getDenominador());
+        
+        return new Fraccion((uno.getNumerador()*dos.getDenominador())+(dos.getNumerador()*uno.getDenominador()), uno.getDenominador()*dos.getDenominador());
     }
     
     public static int MCD(int numerador, int denominador) {
-    return denominador == 0 ? numerador : MCD(denominador, numerador % denominador);
+        
+        return denominador == 0 ? numerador : MCD(denominador, numerador % denominador);
     }
 
     public static Fraccion simplifica(Fraccion uno) {
-    long MCD = MCD(uno.getNumerador(), uno.getDenominador());
+        
+        long MCD = MCD(uno.getNumerador(), uno.getDenominador());
         return new Fraccion((int)(uno.getNumerador()/MCD),(int)(uno.getDenominador()/MCD));
     }
 
